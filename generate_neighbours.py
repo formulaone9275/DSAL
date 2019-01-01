dce028da0d1b2008530b3bf8b5272b4232573fac
Peng Su <psu@udel.edu>
2019-01-01 15:18:58 -0500

    edited

--- generate_neighbours.py
+++ generate_neighbours.py
@@ -1,10 +1,13 @@
-import sent2vec
+#import sent2vec
 from build_tfrecord_data_new import read_sentences,sentence_sequence,load_pickle
 import pickle
 from numpy import dot
 from math import sqrt
 import numpy as np
-
+from cython.parallel import prange
+from tqdm import tqdm
+from multiprocessing import Pool
+from functools import partial
 def cosine_similarity(x,y):
     if sqrt(dot(x,x))==0 or sqrt(dot(y,y))==0:
         return 0
@@ -32,22 +35,63 @@ def get_similarity_score(sent_pickle,dep_pickle,alpha):
     #load the pickle files
     sent_mx=load_pickle('',sent_pickle)
     dep_mx=load_pickle('',dep_pickle)
+    sent_num=len(sent_mx)
+    score_dic={}
+
+    #calculate the similarity
+    for ii in tqdm(prange(sent_num)):
+        # ii means the ii-th sentence
+        similarity_arr=np.zeros(sent_num)
+        similarity_arr=list(similarity_arr)
+        for jj in range(sent_num):
+            if jj==ii:
+                similarity_arr[jj]=1
+            else:
+                similarity_arr[jj]=alpha*cosine_similarity(sent_mx[ii],sent_mx[jj])+(1-alpha)*cosine_similarity(dep_mx[ii],dep_mx[jj])
+
+        top_k_nearest=np.argsort(similarity_arr)[-100:]
+        top_k_score=[similarity_arr[i] for i in top_k_nearest]
+        score_dic[str(ii)]=(top_k_nearest,top_k_score)
+        #print(score_dic[str(ii)])
+    with open('./data/ds/score_dic.pickle', 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump(score_dic, f, pickle.HIGHEST_PROTOCOL)
+
+    return score_dic
+
+
+#define a new function for the for loop
+def cal_sim_arr(index2,index1,score_list,alpha_inside,sent_pickle,dep_pickle):
+
+    #load the pickle files
+    sent_mx=load_pickle('',sent_pickle)
+    dep_mx=load_pickle('',dep_pickle)
+
+    overall_score=alpha_inside*cosine_similarity(sent_mx[index1],sent_mx[index2])+(1-alpha_inside)*cosine_similarity(dep_mx[index1],dep_mx[index2])
+    score_list[index2]=overall_score
+    return score_list
+
+def get_similarity_score_mp(sent_pickle,dep_pickle,alpha):
+
+
+
+    sent_mx_=load_pickle('',sent_pickle)
+    sent_num=len(sent_mx_)
+    #build the similarity matrix first and then generate the score_dic
 
     score_dic={}
-    sent_num=len(sent_mx)
     #calculate the similarity
-    for ii in range(sent_num):
+    for ii in tqdm(range(sent_num)):
         # ii means the ii-th sentence
-        score_dic[str(ii)]=[]
-        for jj in range(sent_num):
-            if jj<ii:
-                score_dic[str(ii)].append(score_dic[str(jj)][ii])
-            elif jj==ii:
-                score_dic[str(ii)].append(1)
-            else:
-                overall_score=alpha*cosine_similarity(sent_mx[ii],sent_mx[jj])+(1-alpha)*cosine_similarity(dep_mx[ii],dep_mx[jj])
-                score_dic[str(ii)].append(overall_score)
-    with open('./data/ds/score_dic.pickle', 'wb') as f:
+        similarity_arr=np.zeros(sent_num)
+        similarity_arr=list(similarity_arr)
+        with Pool(processes=10) as pool:
+            similarity_arr = pool.map(partial(cal_sim_arr, index1=ii,score_list=similarity_arr,alpha_inside=alpha,sent_pickle=sent_pickle,dep_pickle=dep_pickle), [ind for ind in range(sent_num)])
+            top_k_nearest=np.argsort(similarity_arr)[-100:]
+            top_k_score=[similarity_arr[i] for i in top_k_nearest]
+            print('ii',ii)
+        score_dic[str(ii)]=(top_k_nearest,top_k_score)
+    with open('./data/ds/similarity_dic.pickle', 'wb') as f:
         # Pickle the 'data' dictionary using the highest protocol available.
         pickle.dump(score_dic, f, pickle.HIGHEST_PROTOCOL)
 
@@ -60,7 +104,7 @@ def update_label(score_dic,labels,k):
     while max_iter>0:
         #find the k nearest neightbours
         for ni in score_dic.keys():
-            top_k_nearest=np.argsort(score_dic[ni][-1*k:])
+            top_k_nearest=np.argsort(score_dic[ni])[-1*k:]
             #get the label of top k
             neighbour_label_set=[]
             for ti in top_k_nearest:
